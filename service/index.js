b3a80694274a086516fad7415a96098b8991384e
Robin Malfait <malfait.robin@gmail.com>
2017-07-14 20:45:43 +0200

    add micro service example

---
+++ service/index.js
@@ -0,0 +1,17 @@
+const { applyMiddleware } = require("micro-middleware");
+const { validate } = require("..");
+
+async function service(req, res) {
+  const email = req.url.substr(1);
+
+  try {
+    return { email, is_valid: await validate(email) };
+  } catch (err) {
+    return { email, is_valid: false, message: err.message };
+  }
+}
+
+module.exports = applyMiddleware(service, [
+  require("micro-cors")(),
+  require("micro-compress")
+]);
